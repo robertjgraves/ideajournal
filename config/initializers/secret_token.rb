eef2492fb4d522b12c4a5299642d5d7aaf103e08
Robert Graves <robertjgraves@gmail.com>
2013-11-24 22:32:47 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideajournal::Application.config.secret_key_base = '61e72a926eeb32f1e3288b0a0553fadaa8bd3d8ff585ce54c29fc9ba2eec1936f064bb471e2800dba3cb759299912d58262628591a703872f346d2e8a85179f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ideajournal::Application.config.session_store :cookie_store, key: '_ideajournal_session'
